7fc6efc6f0d5d97ae1c6efd2ed95796cd3421113
Esa Adama <you@example.com>
2018-10-07 00:53:04 +0700

    first commit
    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import logo from '../assets/img/logo.png';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+ } from 'reactstrap';
+ import { FaUser } from 'react-icons/fa';
+
+export default class Header extends React.Component {
+  constructor(props) {
+
+    super(props);
+
+    
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false,
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div className="fixed-top">
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand href="/"><img src={logo} alt="logo" /></NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <p>John Doe</p>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
